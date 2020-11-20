3ec6e287c2655f9166c66f40391d43dc9e8380ac
Onallnaitsabes <onallnaitsabes@protonmail.com>
2020-11-20 10:46:43 -0300

    V4

--- prueba1.py
+++ prueba1.py
@@ -1,8 +1,9 @@
 #prueba 1
 
-#Versión V1.0.3: versión inicial
-#Comentario corrección de texto
+#Versión V1.0.4: versión inicial
+#Comentario Linea nueva
 #Creador: Seb
 #Imprimir "Hola mundo"
 
 print ("Hola mundo, como esta? bien y tu? nonono")
+print ("Nuevo")
